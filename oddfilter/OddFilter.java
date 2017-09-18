3bcffa4d4c6f883371d2571f61a04a2d0230b6fd
Alex Toth <drive_alex@hotmail.com>
2017-09-18 15:22:20 +0200

    TrialExamOddFilter

--- oddfilter/OddFilter.java
+++ oddfilter/OddFilter.java
@@ -1,11 +1,9 @@
-import java.util.Arrays;
 
-public class OddFilter {
 
-  public static void main(String[] args) {
-    // Create a function that takes a list as a parameter,
-    // and returns a new list with every odd element from the orignal list
-    System.out.println(oddFilter(Arrays.asList(1, 2, 3, 4, 5)));
-    // should print [1, 3, 5]
-  }
-}
+        // Create a function that takes a list as a parameter,
+        // and returns a new list with every odd element from the orignal list
+
+        //System.out.println(oddFilter(Arrays.asList(1, 2, 3, 4, 5)));
+
+        // should print [1, 3, 5]
+
